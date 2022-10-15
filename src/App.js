1cbbcc5ebb7a4031ec18325b64c2fb1a71804b98
Jenny Watts <93653195+jennie350@users.noreply.github.com>
2022-10-15 16:00:35 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://img.freepik.com/premium-vector/cute-bear-wall-cartoon_42750-669.jpg?w=1060" className="App-logo" alt="logo" />
+      </header>
+
+      <footer>
+        Open source code by <a href="https://github.com/jennie350"target="_blank" rel="noreferrer">Jenny Watts</a>
+      </footer>
+    </div>
+
+  );
+}
+
+export default App;
